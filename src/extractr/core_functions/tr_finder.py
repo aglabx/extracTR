3c69024264b9f5e0b57091c71bd0425bf398f7f7
Aleksey Komissarov <ad3002@gmail.com>
2024-09-23 13:35:14 +0300

    add bisearch

--- src/extractr/core_functions/tr_finder.py
+++ src/extractr/core_functions/tr_finder.py
@@ -8,6 +8,8 @@
 from collections import defaultdict
 from trseeker.tools.sequence_tools import get_revcomp
 from tqdm import tqdm
+from collections import deque
+
 
 def naive_tr_finder(sdat, kmer2tf, min_tf_extension=3000, min_fraction_to_continue=30, k=23):
     ### TODO: save forks decisions
@@ -163,4 +165,162 @@ def tr_greedy_finder(sdat, kmer2tf, max_depth=30_000, coverage=30, min_fraction_
         repeats.append((status, second_status, next_rid, next_i, "".join(seq)))
         rid += 1
 
+    return repeats
+
+
+def tr_greedy_finder_bidirectional(sdat, kmer2tf, max_depth=30_000, coverage=30, min_fraction_to_continue=30, k=23):
+    """
+    Extends sequences in both left and right directions except for TRs.
+    
+    Parameters:
+        sdat: List of tuples (kmer, tf) sorted by tf descending.
+        kmer2tf: Dictionary mapping k-mer to its tf value.
+        max_depth: Maximum length to extend.
+        coverage: Coverage threshold.
+        min_fraction_to_continue: Minimum fraction to continue extending.
+        k: Length of k-mers.
+    
+    Returns:
+        List of repeats with their status and sequences.
+    """
+    
+    MIN_TF = coverage * 100
+    
+    repeats = []
+    rid = 0
+    
+    alphabet = ["A", "C", "T", "G"]
+    cache = {}
+    print("Expected iterations:", len([x for x in sdat if x[1] > MIN_TF]))
+    
+    for (start_kmer, tf) in tqdm(sdat):
+        if tf < MIN_TF:
+            break
+        if start_kmer in cache:
+            continue
+        
+        # Initialize for bidirectional extension
+        seq_deque = deque([start_kmer])
+        status = None
+        second_status = None
+        next_rid = None
+        next_i = None
+        total_length = k
+        
+        # Mark the start k-mer in cache
+        cache[start_kmer] = (rid, 0, total_length)
+        
+        # Right extension
+        right_prefix = start_kmer[1:]
+        right_seq = []
+        right_length = 0
+        right_status = None
+        
+        while right_length < max_depth:
+            solutions = []
+            for nucleotide in alphabet:
+                new_kmer = right_prefix + nucleotide
+                ctf = kmer2tf.get(new_kmer, 0)
+                if ctf < MIN_TF:
+                    continue
+                if ctf:
+                    solutions.append((ctf, nucleotide))
+            if not solutions:
+                right_status = "zero"
+                break
+            
+            # Select the nucleotide with the highest coverage
+            solutions.sort(reverse=True)
+            ctf, nucleotide = solutions[0]
+            new_kmer = right_prefix + nucleotide
+            right_seq.append(nucleotide)
+            total_length += 1
+            
+            if new_kmer == start_kmer:
+                right_status = "tr"
+                break
+            
+            if new_kmer in cache:
+                existing_rid, strand, i = cache[new_kmer]
+                if existing_rid not in repeats:
+                    second_status = "self"
+                else:
+                    second_status = repeats[existing_rid][0]
+                next_rid = existing_rid
+                next_i = i
+                right_status = "frag"
+                break
+            
+            # Update for next iteration
+            cache[new_kmer] = (rid, 0, total_length)
+            right_prefix = new_kmer[1:]
+            right_length += 1
+        
+        # If TR detected in right extension, skip left extension
+        if right_status == "tr":
+            final_status = "tr"
+            full_seq = ''.join(right_seq)
+        else:
+            # Left extension
+            left_suffix = start_kmer[:-1]
+            left_seq = []
+            left_length = 0
+            left_status = None
+            
+            while left_length < max_depth:
+                solutions = []
+                for nucleotide in alphabet:
+                    new_kmer = nucleotide + left_suffix
+                    ctf = kmer2tf.get(new_kmer, 0)
+                    if ctf < MIN_TF:
+                        continue
+                    if ctf:
+                        solutions.append((ctf, nucleotide))
+                if not solutions:
+                    left_status = "zero"
+                    break
+                
+                # Select the nucleotide with the highest coverage
+                solutions.sort(reverse=True)
+                ctf, nucleotide = solutions[0]
+                new_kmer = nucleotide + left_suffix
+                left_seq.append(nucleotide)
+                total_length += 1
+                
+                if new_kmer == start_kmer:
+                    left_status = "tr"
+                    break
+                
+                if new_kmer in cache:
+                    existing_rid, strand, i = cache[new_kmer]
+                    if existing_rid not in repeats:
+                        second_status = "self"
+                    else:
+                        second_status = repeats[existing_rid][0]
+                    next_rid = existing_rid
+                    next_i = i
+                    left_status = "frag"
+                    break
+                
+                # Update for next iteration
+                cache[new_kmer] = (rid, 0, total_length)
+                left_suffix = new_kmer[:-1]
+                left_length += 1
+            
+            # Determine overall status
+            if left_status == "tr" or right_status == "tr":
+                final_status = "tr"
+            elif left_status == "frag" or right_status == "frag":
+                final_status = "frag"
+            elif left_status == "zero" and right_status == "zero":
+                final_status = "zero"
+            else:
+                final_status = "extended"
+            
+            # Combine left and right sequences
+            full_seq = ''.join(reversed(left_seq)) + start_kmer + ''.join(right_seq)
+        
+        repeats.append((final_status, second_status, next_rid, next_i, full_seq))
+        rid += 1
+    
     return repeats
